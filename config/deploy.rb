2de808d9c05f69a836d76ef40b10ef8626fa93e3
Stanislav Gernyak <stager94@mail.ru>
2016-09-25 00:02:09 +0300

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,12 +55,12 @@ namespace :deploy do
     run "cp #{deploy_to}/shared/database.yml #{release_path}/config/database.yml"
   end
 
-  # namespace :assets do
-  #   task :precompile, :roles => assets_role, :except => { :no_release => true } do
-  #     run <<-CMD.compact
-  #       cd -- #{latest_release.shellescape} &&
-  #       #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile
-  #     CMD
-  #   end
-  # end
+  namespace :assets do
+    task :precompile, :roles => assets_role, :except => { :no_release => true } do
+      run <<-CMD.compact
+        cd -- #{latest_release.shellescape} &&
+        #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile
+      CMD
+    end
+  end
 end
