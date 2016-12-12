159be2845e76b040a1577865cc32576cd4b91c4b
Stanislav Gernyak <stager94@mail.ru>
2016-12-13 00:16:58 +0200

    fix uploader for campaigns cover to system folder

--- app/uploaders/campaigns/cover_uploader.rb
+++ app/uploaders/campaigns/cover_uploader.rb
@@ -7,7 +7,7 @@ class Campaigns::CoverUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url(*args)
