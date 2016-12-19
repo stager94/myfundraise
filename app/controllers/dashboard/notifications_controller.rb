7730350314aa0fa87950444183ec69a58b67e469
Stanislav Gernyak <stager94@mail.ru>
2016-12-20 01:46:48 +0200

    -

--- app/controllers/dashboard/notifications_controller.rb
+++ app/controllers/dashboard/notifications_controller.rb
@@ -3,12 +3,12 @@ class Dashboard::NotificationsController < ApplicationController
 	PAGE_SIZE = 20
 
 	POSSIBLE_TABS = %w(all donation comment like)
-	
+
 	def index
 		@tab = current_tab
 		scope = PublicActivity::Activity.where(recipient_id: current_user.id, recipient_type: "User")
 		scope = scope.where "key ILIKE ?", "#{@tab}.%" if @tab != POSSIBLE_TABS.first
-																						 
+
 		@notifications = scope.order(created_at: :desc).page(params[:page]).per PAGE_SIZE
 	end
 
