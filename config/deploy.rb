908f68013f64f45d202cd31cb97c2d65b12ed61c
Stanislav Gernyak <stager94@mail.ru>
2017-03-06 01:50:40 +0200

    fix google code

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :user,        'root'
 set :use_sudo,    false
 
 set :scm, :git
-set :repository, "git@github.com:MarRyb/myfundraise.git"
+set :repository, "git@github.com:stager94/myfundraise.git"
 set :deploy_via, :remote_cache
 
 set(:unicorn_conf) { "#{deploy_to}/current/config/unicorn/#{fetch(:stage)}.rb" }
